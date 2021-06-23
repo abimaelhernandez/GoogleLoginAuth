9998635bdfdafcc91c385f9313f6f3bee04a0e2b
abimaelhernandez <hernandezabimael364@gmail.com>
2021-06-22 19:39:48 -0500

    simple api response

--- server/config.js
+++ server/config.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.use(express.static(__dirname + "/../client/dist"));
+// app.use(express.static(__dirname + "/../client/dist"));
+app.use((req, res, next) => {
+  res.status(200).json({
+    message: 'Simple REST API res'
+  })
+});
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-const app = require('./config.js');
+const app = require('./config.js')
+const http = require('http')
+const port = process.env.PORT || 5000
 
-const port = process.env.PORT || 5000;
-app.listen(port, function ()  {
-  console.log(`Listening on Port ${port}`);
-});
+const server = http.createServer(app)
+
+server.listen(port, () => {
+  console.log(`Listening on Port ${port}`)
+})
