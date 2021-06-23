9c85bede490f022b6b1eb78e4f788e765760e477
abimaelhernandez <hernandezabimael364@gmail.com>
2021-06-23 13:27:15 -0500

    enable cors so it can be viewed buy anyone making a request

--- server/config.js
+++ server/config.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const app = express();
 const path = require('path')
+const cors = require('cors')
+app.use(
+  cors({
+    origin: "*"
+  })
+)
 
-// app.use(express.static(__dirname + "/../client/dist"));
 app.use((req, res, next) => {
+  // secondary way to eliminate CORS
+  // res.header("Access-Control-Allow-Origin", "*");
   res.status(200).json({
-    message: 'Simple REST API res'
+    message: 'Simple REST API Is here'
   })
 });
 
